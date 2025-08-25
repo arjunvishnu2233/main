423882c92c5cf54192c96a0cf64cb391dbc8d2b7
arjunvishnuzz2233 <arjunvishnu00@gmail.com>
2025-08-25 17:35:02 +0530

    the file added

--- set.py
+++ set.py
@@ -1,5 +1,4 @@
-my_set={1,2,3,4,5}
-print(my_set)
+
 
 # my_set2= set([12,35,699])
 # print(my_set2)
@@ -8,25 +7,36 @@ print(my_set)
 #     print(x)
 
     #add
-my_set.add((45,47))
-my_set.add((45,47))
+# my_set.add((45,47))
+# my_set.add((45,47))
 
-print(my_set)
+# print(my_set)
 
 #update
 
-my_set.update([45,68,75])
-print(my_set)
+# my_set.update([45,68,75])
+# print(my_set)
 
 #deletion
 
-my_set.pop()
-print(my_set)
+# my_set.pop()
+# print(my_set)
 
-#remove
-my_set.remove(45)
-print(my_set)
+# #remove
+# my_set.remove(45)
+# print(my_set)
 
-my_set.discard("hello")
-my_set.clear()
-print(my_set)
+# my_set.discard("hello")
+# my_set.clear()
+# print(my_set)
+
+# print(len(my_set))
+# print(type(my_set2))
+
+set1={1,3,5,7,9,}
+set2={2,4,6,7,8,10}
+
+# set3=set2.union(set1)
+# set3=set1 | set2
+set3=set1.intersection([1,7])
+print(set3)
