f606bbf2bd54c89e8edb93b2aded9e978f293859
arjunvishnuzz2233 <arjunvishnu00@gmail.com>
2025-08-25 17:54:04 +0530

    the file added

--- set.py
+++ set.py
@@ -38,5 +38,9 @@ set2={2,4,6,7,8,10}
 
 # set3=set2.union(set1)
 # set3=set1 | set2
-set3=set1.intersection([1,7])
-print(set3)
+# set3=set1.intersection([1,7])
+# set3=set1.difference(set2)
+set1.difference_update(set2)
+# print(set3)
+
+
