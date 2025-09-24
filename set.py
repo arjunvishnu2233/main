c52afced760f5fbfcc2cfa6f74fc2e11d2c9ce31
arjunvishnuzz2233 <arjunvishnu00@gmail.com>
2025-09-24 17:26:14 +0530

    first commit

--- set.py
+++ set.py
@@ -33,14 +33,22 @@
 # print(len(my_set))
 # print(type(my_set2))
 
-set1={1,3,5,7,9,}
-set2={2,4,6,7,8,10}
+# set1={1,3,5,7,9,}
+# set2={2,4,6,7,8,10}
 
 # set3=set2.union(set1)
 # set3=set1 | set2
 # set3=set1.intersection([1,7])
 # set3=set1.difference(set2)
-set1.difference_update(set2)
+# set1.difference_update(set2)
 # print(set3)
+
+#is_disjoint
+# print(set1.isdisjoint(set2))
+
+# s1={1,2,3,4,5,6,7,8,9}
+# s2={1,2,3}
+
+# print(s2.issubset(s1),"==> subset")
 
 
