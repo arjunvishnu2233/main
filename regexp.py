202e88195f5085802e513481d7b85e32924edc18
arjunvishnuzz2233 <arjunvishnu00@gmail.com>
2025-09-25 17:32:08 +0530

    the file is added

--- regexp.py
+++ regexp.py
@@ -36,4 +36,30 @@ import re
 
 #re.sub
 
-print(re.sub(r'\d+','X','My phone number is 8848932807'))
+# print(re.sub(r'\d+','X','My phone number is 8848932807'))
+
+#re.split
+# re.split('pattern','string')
+
+#\escape character
+# date=r'20\12\2025'
+# print(re.search(r'\\',date)) 
+
+#capturing groups
+
+# msg='my birthday is 20-12-2025'
+
+# pattern = r'(?P<day>\d{2})-(?P<month>\d{2})-(?P<year>\d{4})'
+
+# match=re.search(pattern,msg)
+
+# print(f'date:{match.group('day')}')
+# print(f'month:{match.group('month')}')
+# print(f'year:{match.group('year')}')
+
+# print(f'date:{match.group(1)}')
+# print(f'month:{match.group(2)}')
+# print(f'year:{match.group(3)}')
+
+1-31
+1-12
